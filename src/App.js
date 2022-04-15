c4eadf46000637d1917627856c3b9440bd6a61bd
Utkarsh <utripathi2002@gmail.com>
2022-04-15 18:38:21 +0530

    Starting to learn on the polygon network

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import {Web3ReactProvider} from '@web3-react/core';
-import {Web3Provider} from '@ethersproject/providers';
+import { Web3ReactProvider } from "@web3-react/core";
+import { Web3Provider } from "@ethersproject/providers";
 
-import Header from './components/Header';
-import Footer from './components/Footer';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
-import Content from './components/Containers';
-import {Container, Image} from 'semantic-ui-react';
-import iconDevx from "./assets/icon-devx.svg"
+import Content from "./components/Containers";
+import { Container, Image } from "semantic-ui-react";
+import iconDevx from "./assets/icon-devx.svg";
 
-import './App.css';
+import "./App.css";
 
 const getLibrary = (provider) => {
   const library = new Web3Provider(provider);
@@ -18,19 +18,33 @@ const getLibrary = (provider) => {
 
 function App() {
   return (
-  <Web3ReactProvider getLibrary={getLibrary}>
-    <div className="App">
-      <Header/>
-      <Content>
-        <Container text textAlign='center'>
-          <Image centered size='tiny' src={iconDevx} style={{marginBottom:"2em"}}/>
-          <p>Create React Ethereum DApp on Polygon</p>
-          <p>Build your first app with Starter Kits</p>
-        </Container>
-      </Content>
-      <Footer/>
-    </div>
-  </Web3ReactProvider>
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <div className="App">
+        <Header />
+        <Content>
+          <Container text textAlign="center">
+            <Image
+              centered
+              size="tiny"
+              src={iconDevx}
+              style={{ marginBottom: "2em" }}
+            />
+            <p
+              style={{
+                fontSize: "2rem",
+                textDecoration: "underline",
+                fontWeight: "bold",
+              }}
+            >
+              Hello, World !! ,I am Utkarsh.
+            </p>
+            <p>I am gonna build the best dApp on the polygon network.</p>
+            <p>Looking forward to learning alot from this course.</p>
+          </Container>
+        </Content>
+        <Footer />
+      </div>
+    </Web3ReactProvider>
   );
 }
 
